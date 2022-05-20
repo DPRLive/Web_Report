cee4957081d13ff13d58f6b861f5e43f92f9f6ed
imusun <imusun190812@gmail.com>
2022-05-21 01:23:48 +0900

    css수정, article 태그 삭제

--- script.js
+++ script.js
@@ -29,18 +29,14 @@ function imgClick()
 let krafton = new String(`
 <img src="imgSrc/Krafton_Logo.png" onclick="imgClick()">
 <header class="subHeader">기업소개</header>
-<article class="subArticle"> 
     크래프톤은 특정 장르에서 경쟁력 있는 제작 능력을 갖춘 독립스튜디오들로 구성되어 있습니다. 
         현재는 ‘펍지 스튜디오’, ‘블루홀스튜디오’, ‘라이징윙스', ‘스트라이킹 디스턴스 스튜디오’, ‘드림모션’과 
         여러 제작팀이 전 세계 게이머들에게 최고의 게임 경험을 제공하기 위해 개발에 임하고 있습니다. 
         배틀로얄 장르의 ‘PUBG: 배틀그라운드(PUBG: BATTLEGROUNDS)’와 '배틀그라운드: NEW STATE(PUBG: NEW STATE)', 
         MMORPG ‘테라(TERA)’, ‘엘리온(ELYON)’과 캐주얼 게임을 PC, 모바일, 콘솔 등 다양한 플랫폼에서 즐길 수 있게 제작하고 있습니다. 
         게임 개발뿐만 아니라 딥러닝과 엔터테인먼트 등 새로운 분야의 사업을 발굴하며 기술 기업으로서 강점도 발휘하고 있습니다.
-        <hr>
-</article> 
-
+<hr>
 <header class="subHeader">근무환경</header>
-<article class="subArticle"> 
         <table class="subTable">
             <tr><th>연금, 보험</th> <td>국민연금, 고용보험, 산재보험, 건강보험</td></tr>
             <tr><th>휴무,휴가,회사</th> <td>주5일근무, 연차, 정기휴가, 경조휴가, 반차, Refresh휴가, 산전 후 휴가, 육아휴직, 워크샵/MT</td></tr>
@@ -49,11 +45,8 @@ let krafton = new String(`
             <tr><th>사내시설, 장애인 지원</th> <td>사내식당, 휴게실, 카페테리아, 탁아시설, 주차장</td></tr>
             <tr><th>생활편의, 여가행사</th> <td>사내 동호회 운영, 통근버스 운행, 건강검진, 중식제공, 석식제공, 휴양시설 지원</td></tr>
         </table>
-        <hr>
-</article> 
-
+<hr>
 <header class="subHeader">채용 공고 (언리얼 엔진 관련)</header>
-<article class="subArticle"> 
     <details>
         <summary>[BATTLEGROUNDS] UE4 Contents Programmer (경력 무관)</summary>
             <p>진행 업무</p>
@@ -78,34 +71,25 @@ let krafton = new String(`
             <li>서류전형 > 전화 인터뷰 (Phone Interview) > 직무 테스트 (Technical Fit Test) >  
                 실무면접 (Technical Fit Interview) > 최종면접 (Culture Fit Interview) > 합격 및 입사</li>
     </details>
-        <hr>
-
+<hr>
         <header class="subHeader">실무자 기업 평가</header>
-            <article class="subArticle"> 
                 "업무 관련된 지원이 많이 되고, 팀 내 분위기가 수평적이고 유연한 편" <br><br>
                 "좋은 분위기에서 일할 수 있는 곳. 사람들이 좋고 보수적인 문화가 없어 열심히 일한다면 마음편하게 일할 수 있을 것입니다."<br><br>
                 "원히트원더 어쩌다 성공했다보니 개발력이 많이 부족함. 펍지는 모르겠지만 크래프톤에는 블루홀출신 고인물들이 너무 많음"<br><br>
                 "내가 할일만 잘하면 다른 외부 스트레스 없이 다닐 수 있었음"<br><br>
                 "전체적으로 직원들이 한숨보다 즐겁게 근무하는 곳"
-    </article> 
-</article> 
-</article>
 `);
 
 let nexon = new String(`
 <img src="imgSrc/Nexon_Logo.png" onclick="imgClick()">
 <header class="subHeader">기업소개</header>
-<article class="subArticle"> 
     1994년 설립, 올해로 창립 20주년을 맞이한 (주)넥슨은 연 매출 2조 8천억원,(2013년 말 넥슨 일본법인 연결매출 기준) 사원수 3,500명 규모의 글로벌 게임회사 입니다.
     2002년 일본, 2005년 미국, 2007년 유럽에 현지법인을 설립하며 일찌감치 해외 시장 개척에 앞장서온 넥슨은 현재 일본,중국,대만 등 
     동아시아 3국을 비롯해 북미지역과 유럽, 남미 등 전 세계 110여개국에 진출해 150여 개의 게임을 서비스 중이며, 현재 14억 명의 
     사용자(계정)을 확보하고 있습니다. 경기도 성남시 분당구 판교로에 위치하고 있으며, 온라인, 모바일, 소셜 게임의 개발 및 서비스 
     사업을 하고 있습니다.
-        <hr>
-</article> 
-
+<hr>
 <header class="subHeader">근무환경</header>
-<article class="subArticle"> 
         <table class="subTable" id="nexonTable">
             <tr><th>쾌적한 근로환경</th> <td>의자계의 샤넬, ‘허먼밀러 에어론’ 기본 탑재</td></tr>
             <tr><th>선택적 근로시간제도</th> <td>월 근로시간만 채우면 자율적으로 근로시간 조정하여 근무</td></tr>
@@ -137,11 +121,8 @@ let nexon = new String(`
             <tr><th>사내외교육</th> <td>성장하고 싶은 넥슨인을 위한 다양한 교육을 적극 지원</td></tr>
             <tr><th>추천채용</th> <td>외부의 좋은 인재 추천시 직원에게 최대 200만원까지 보상금 지급</td></tr>
         </table>
-        <hr>
-</article> 
-
-<header class="subHeader">채용 공고 (언리얼 엔진 관련)</header>
-<article class="subArticle"> 
+<hr>
+<header class="subHeader">채용 공고 (언리얼 엔진 관련)</header> 
     <details>
         <summary>[프로젝트 HP] 게임프로그래머(클라이언트)</summary>
             <p>진행 업무</p>
@@ -195,16 +176,11 @@ let nexon = new String(`
             <li>UnrealEngine4 또는 UnrealEngine5 UI 구현 경험</li>
             <li>원활한 커뮤니케이션 능력</li>
     </details>
-        <hr>
-
+<hr>
         <header class="subHeader">실무자 기업 평가</header>
-            <article class="subArticle"> 
             "복지가 괜찮은 편이며 큰 욕심 없이 무난하게 다니기 좋음." <br><br>
             "공무원처럼 다닐 수 있는 나름 젊은(?) 게임회사"<br><br>
             "중소게임사에서 대기업이 된, 변화 과정에의 진통을 잘 이겨낸 철밥통st의 IT 기업"<br><br>
             "팀바팀이겠지만 개발자로서 성장하기 좋은 환경은 아니라고 생각함."<br><br>
-            "넉넉한 자금력으로 직원들의 복지 혜택을 넓히는 미래지향적인 회사"
-    </article> 
-</article> 
-</article>
+            "넉넉한 자금력으로 직원들의 복지 혜택을 넓히는 미래지향적인 회사" 
 `)
